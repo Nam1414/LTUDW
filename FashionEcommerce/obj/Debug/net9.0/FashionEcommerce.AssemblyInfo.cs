d7c619f6d458a4bbbb660e92212f28048dae8076
kha_ta <khatong072@gmail.com>
2026-03-12 10:01:06 +0700

    Upload Back-end

--- FashionEcommerce/obj/Debug/net9.0/FashionEcommerce.AssemblyInfo.cs
+++ FashionEcommerce/obj/Debug/net9.0/FashionEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FashionEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7eae369a0922cdbeb420ca06ef0e5a7863b8232")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99c8b9daf6c9a3e7d47755499fd348be522b54bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("FashionEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FashionEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
