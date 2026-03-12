99c8b9daf6c9a3e7d47755499fd348be522b54bd
kha_ta <khatong072@gmail.com>
2026-03-12 09:48:31 +0700

    Update Readme

--- FashionEcommerce/obj/Debug/net9.0/FashionEcommerce.AssemblyInfo.cs
+++ FashionEcommerce/obj/Debug/net9.0/FashionEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FashionEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cea16627ca309e7861002df75b4671d2c0a71e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7eae369a0922cdbeb420ca06ef0e5a7863b8232")]
 [assembly: System.Reflection.AssemblyProductAttribute("FashionEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FashionEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/FashionEcommerce.Api/Services/IJwtService.cs
+++
@@ -1,8 +0,0 @@
-using FashionEcommerce.Api.Models;
-
-namespace FashionEcommerce.Api.Services;
-
-public interface IJwtService
-{
-    string GenerateToken(User user);
-}
